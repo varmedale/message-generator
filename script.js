a9130fce7270e8cfb10401982cf29ca9b3766744
varmedale <evitatlfw@hotmail.com>
2022-04-14 17:08:13 +0200

    har lagt til tilfeldig valg av element

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const weather = ["sol", "snø", "regn", "yr", "tåke", "minusgrader", "frost", "
 const moods = ["glad", "gal", "sur", "bisk", "irritert", "sint", "engasjert", "deprimert", "lykkelig", "sliten", "sulten", "trøtt", "konsentrert",]
 
 function textGenerator(){
-    const genre = genres[0];
-    const currentWeather = weather[0];
-    const mood = moods[0];
+    const genre = genres[Math.floor(Math.random()*genres.length)];
+    const currentWeather = weather[Math.floor(Math.random()*weather.length)];
+    const mood = moods[Math.floor(Math.random()*moods.length)];
     console.log("idag er det fint å høre på "+genre+", siden det er "+currentWeather+" og du blir "+mood+".");
 
 }
