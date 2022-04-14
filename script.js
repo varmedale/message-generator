239d8cd6c093fd64d0f577f12c39e781d8b5925a
varmedale <evitatlfw@hotmail.com>
2022-04-14 16:26:56 +0200

    har lagt til vær

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const genres = ["rock", "pop", "punk", "klassisk", "country", "folkemusikk", "rap", "hip-hop", "indie", "metal", "jazz", "soul", "blues", "funk", "k-pop", "gospel", "julemusikk", "latin"];
-
+const weather = ["sol", "snø","regn","yr","tåke","minusgrader","frost","overskyet","storm","orkan"];
 
 
 function textGenerator(){
     const genre = genres[0];
-    console.log("idag er det fint å høre på "+genre+", siden det er sol.");
+    const currentWeather = weather[0];
+    console.log("idag er det fint å høre på "+genre+", siden det er "+currentWeather+" og humøret ditt blir bedre");
 
 }
 
